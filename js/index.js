66f4c6cfc3cb46099e0838e2e65c754bdc9fd8ce
Smokc24 <santistebanmariana21@gmail.com>
2025-04-01 15:07:26 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+function Loguear() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('clave').value
+
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    if (user == null) {
+        alert('No has iniciado sesion')
+        window.location.href = "../index.html"
+    } else if (user[2] == username && user[3] == password) {
+        localStorage.setItem("user", username);
+        window.location.href = "../vistas/Entrenador.html"
+        alert('Usuario valido')
+    }
+
+}
+
+
